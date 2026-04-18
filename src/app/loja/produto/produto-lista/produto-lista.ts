6fd0d430ffb22e377649fdc0f3ef00c3f3cfa891
LuisFernandoChagas <lflchagas@gmail.com>
2026-04-18 14:21:47 -0300

    feat: Tela de listar produtos

--- src/app/loja/produto/produto-lista/produto-lista.ts
+++ src/app/loja/produto/produto-lista/produto-lista.ts
@@ -9,12 +9,58 @@ import { CommonModule } from '@angular/common';
 })
 export class ProdutoLista {
   produtos = [
-    { id: 1, imagem: '/celular.webp', nome: 'Celular', preco: 0, quantidade: 1 },
-    { id: 2, imagem: '/notebook.webp', nome: 'Notebook', preco: 0, quantidade: 1 },
-    { id: 3, imagem: '/televisao.webp', nome: 'Televisão', preco: 0, quantidade: 1 },
+    {
+      id: 1,
+      imagem: '/celular.webp',
+      produto: {
+        nome: 'Celular',
+        codigo: 123456,
+        cor: 'Branco',
+      },
+      preco: 1000,
+      quantidade: 0
+    },
+    {
+      id: 2,
+      imagem: '/notebook.webp',
+      produto: {
+        nome: 'Notebook',
+        codigo: 789456,
+        cor: 'Preto',
+      },
+      preco: 2500,
+      quantidade: 0
+    },
+    {
+      id: 3,
+      imagem: '/televisao.webp',
+      produto: {
+        nome: 'Televisão',
+        codigo: 123456,
+        cor: 'Cinza',
+      },
+      preco: 2000,
+      quantidade: 0
+    }
   ];
 
-  get subtotal() {
-    return this.produtos.reduce((sum, produto) => sum + (produto.preco * produto.quantidade), 0);
+  subtotal: number = 0;
+
+  calcularSubtotal() {
+    this.subtotal = this.produtos.reduce((sum, produto) => sum + (produto.preco * produto.quantidade), 0);
+  }
+
+  retirar(item: any) {
+    if (item.quantidade > 0) {
+      item.quantidade--;
+    }
+
+    this.calcularSubtotal();
+  }
+  
+  adicionar(item: any) {
+    item.quantidade++;
+
+    this.calcularSubtotal();
   }
 }
